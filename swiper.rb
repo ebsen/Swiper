19f3f4671ed2ba3b2ec1ee44abdc23ab319c1592
Erik L. Ebsen <erik.ebsen@sdstate.edu>
2013-03-27 13:01:54 -0500

    Sucessfully swipes contents of 'images' array

--- swiper.rb
+++ swiper.rb
@@ -1,7 +1,28 @@
 #!/usr/bin/env ruby -wKU
+require "open-uri"
 
 puts "Swiper, no swiping!"
 puts "Swiper, no swiping!"
 puts "Swiper, no swiping!"
-puts "\n"
+puts "..."
+
+# open('image.png', 'wb') do |file|
+#   file << open('http://igrow.org/up/authors/Dunn.Barry-iGrow.jpg').read
+# end
+
+images = [
+  'http://igrow.org/up/authors/Dunn.Barry-iGrow.jpg',
+  'http://igrow.org/up/authors/Burke.Shawn-iGrow.jpg',
+  'http://igrow.org/up/authors/Edwards.Laura-iGrow.jpg',
+  'http://igrow.org/up/authors/Salverson.Robin-iGrow.jpg'
+]
+
+images.each do |url|
+  name = url.split "/"
+  file_name = name.last.inspect.gsub!(/"/, "").gsub!(/-iGrow/, "")
+  open( file_name, 'wb' ) do |file|
+    file << open(url).read
+  end
+end
+
 puts "Aww, man!"

73fad4dbcf81b7e69e0fd61c70418aa988359115
Erik L. Ebsen <erik.ebsen@sdstate.edu>
2013-04-03 11:41:25 -0500

    Script now opens a SQLite3 database, queries it for each person's _id, and uses that as the image name.

--- swiper.rb
+++ swiper.rb
@@ -1,12 +1,9 @@
 #!/usr/bin/env ruby -wKU
 require "open-uri"
+require "sqlite3"
 
-puts "Swiper, no sw--!"
+puts "Swiper, no swiping!"
 puts "..."
-
-# open('image.png', 'wb') do |file|
-#   file << open('http://igrow.org/up/authors/Dunn.Barry-iGrow.jpg').read
-# end
 
 images = [
   'http://igrow.org/up/authors/Dunn.Barry-iGrow.jpg',
@@ -14,13 +11,33 @@ images = [
   'http://igrow.org/up/authors/Edwards.Laura-iGrow.jpg',
   'http://igrow.org/up/authors/Salverson.Robin-iGrow.jpg'
 ]
+db = SQLite3::Database.open( "directory_data.db" )
 
+# Get the images and save them into this script's directory.
 images.each do |url|
+
   name = url.split "/"
   file_name = name.last.inspect.gsub!(/"/, "").gsub!(/-iGrow/, "")
+
+  last, first, file_extension = file_name.split( "." )
+  person_info = db.get_first_row( "SELECT _id, last_name, first_name_pref FROM People WHERE last_name = ? AND first_name_pref = ?", [last, first] )
+  puts person_info
+  person_id = person_info.first
+  # Write contents to file_name
+  file_name = person_id.to_s << "." << file_extension
+  puts file_name
+  # 
   open( file_name, 'wb' ) do |file|
-    file << open(url).read
+    file << open(url).read  
   end
+    
 end
 
-puts "You'll never find your images now!"
+# When we're done, close the database object.
+db.close unless db.closed?
+
+# if $stderr
+#   puts "Swiper, no swiping! Swiper, no swiping!"
+#   puts "Ohhh, man!"
+# end
+puts "You're too late! You'll never find your images now!"
